3096547d8f637670a42be653c7e54d90412faab0
neligerova <ntaneva92@gmail.com>
2017-11-30 20:33:44 +0200

    Initial commit

---
+++ bundle/bin/listen.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"c:\Ruby23\bin\ruby.exe" "c:/Users/Neli/Ruby on Rails/Ruby_apps/academy-app/bundle/bin/listen" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"c:\Ruby23\bin\ruby.exe" "%~dpn0" %*
